35585b9cecfe3da2455eccc94fa23e1f414f4531
#云淡然 <cloudcome@163.com>
2016-05-20 17:53:41 +0800

    blear.ui

---
+++ readme.md
@@ -0,0 +1,15 @@
+# blear.ui
+
+[![npm module][npm-img]][npm-url]
+[![build status][travis-img]][travis-url]
+[![coverage][coveralls-img]][coveralls-url]
+
+[travis-img]: https://img.shields.io/travis/blearjs/blear.ui/master.svg?maxAge=2592000&style=flat-square
+[travis-url]: https://travis-ci.org/blearjs/blear.ui
+
+[npm-img]: https://img.shields.io/npm/v/blear.ui.svg?maxAge=2592000&style=flat-square
+[npm-url]: https://www.npmjs.com/package/blear.ui
+
+[coveralls-img]: https://img.shields.io/coveralls/blearjs/blear.ui/master.svg?maxAge=2592000&style=flat-square
+[coveralls-url]: https://coveralls.io/github/blearjs/blear.ui?branch=master
+
---
+++ src/index.js
@@ -0,0 +1,41 @@
+/**
+ * UI 基类
+ * @author ydr.me
+ * @create 2016-04-19 11:08
+ */
+
+
+
+'use strict';
+
+var Events = require('blear.classes.events');
+
+var zIndex = 9999;
+
+var UI = Events.extend({
+    className: 'UI',
+    constructor: function UI() {
+        this.Super();
+    },
+
+    destroy: function () {
+        this.Super.destroy('destroy');
+    }
+});
+
+/**
+ * 申请层级值
+ * @returns {number}
+ */
+UI.zIndex = function () {
+    return zIndex++;
+};
+
+UI.BUTTON_CLASS = 'dkbtn';
+UI.TEXT_CLASS = 'dktext';
+UI.INPUT_CLASS = 'dkipt';
+UI.UI_CLASS = 'dkui';
+require('./_style/base.css', 'css|style');
+require('./_style/input.css', 'css|style');
+require('./_style/button.css', 'css|style');
+module.exports = UI;
