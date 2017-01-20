baaacf91536e6c0eca1ce73cc004e481cc6eca73
#云淡然 <cloudcome@163.com>
2017-01-20 15:58:51 +0800

    + build get/set option

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@
 'use strict';
 
 var Events = require('blear.classes.events');
+var object = require('blear.utils.object');
 
 var zIndex = 9999;
 
@@ -31,11 +32,52 @@ UI.zIndex = function () {
     return zIndex++;
 };
 
+/**
+ * @function getOption
+ * @param val
+ */
+
+/**
+ * 生成一个 get options 方法
+ * @param ui
+ * @param optionsName
+ * @returns {getOption}
+ */
+UI.buildGetOption = function (ui, optionsName) {
+    return function getOption(key) {
+        if (!key) {
+            return ui[optionsName];
+        }
+
+        return object.get(ui[optionsName], key);
+    };
+};
+
+/**
+ * @function setOption
+ * @param key
+ * @param val
+ */
+
+/**
+ * 生成一个 set options 方法
+ * @param ui
+ * @param optionsName
+ * @returns {setOption}
+ */
+UI.buildSetOption = function (ui, optionsName) {
+    return function setOption(key, val) {
+        object.set(ui[optionsName], key, val);
+    };
+};
+
 UI.BUTTON_CLASS = 'blearbtn';
 UI.TEXT_CLASS = 'bleartext';
 UI.INPUT_CLASS = 'bleaript';
 UI.UI_CLASS = 'blearui';
+
 require('./_style/base.css', 'css|style');
 require('./_style/input.css', 'css|style');
 require('./_style/button.css', 'css|style');
+
 module.exports = UI;
